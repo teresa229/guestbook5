1b66390726cc1d0730d40039f6ad1f1d0c26d5d1
teresa229 <scarlett_29@naver.com>
2021-01-26 04:51:33 +0900

    미완성

--- src/main/java/com/javaex/controller/GuestController.java
+++ src/main/java/com/javaex/controller/GuestController.java
@@ -8,7 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.javaex.dao.GuestDao;
 import com.javaex.vo.GuestVo;
@@ -16,7 +15,6 @@ import com.javaex.vo.GuestVo;
 @Controller
 @RequestMapping(value="/guestbook")
 public class GuestController{
-	
 	
 	//필드
 	@Autowired
@@ -26,9 +24,9 @@ public class GuestController{
 	public String list(Model model){   
 		System.out.println("list");
 		
-		//GuestDao guestDao = new GuestDao();
 		List<GuestVo> guestList = guestDao.ListAllGuest(); 
 		System.out.println(guestList.toString());
+		
 		//model에 담아 보내기
 		model.addAttribute("gList", guestList);
 		
@@ -42,7 +40,6 @@ public class GuestController{
 		System.out.println("insert");
 
 		System.out.println(guestVo.toString());
-		//GuestDao guestDao = new GuestDao();
 		
 		/* insert 저장 */
 		guestDao.guestInsert(guestVo);
@@ -51,23 +48,6 @@ public class GuestController{
 		return "redirect:/guestbook/list";
 	}
 	
-	// @RequestParam
-	@RequestMapping(value="/insert2", method= {RequestMethod.GET, RequestMethod.POST})
-	public String insert2(@RequestParam("name")String name, @RequestParam("password")String password, @RequestParam("content")String content){
-		System.out.println("insert2");
-
-		System.out.println(name + "," + password + "," + content);
-		
-		GuestVo guestVo= new GuestVo(name, password, content);
-		System.out.println(guestVo.toString());
-		//GuestDao guestDao = new GuestDao();
-		
-		/* insert 저장 */
-		guestDao.guestInsert(guestVo);
-		
-		/* 리다이렉트 */		
-		return "redirect:/guestbook/list";
-	}
 		
 	@RequestMapping(value="/deleteForm",method= {RequestMethod.GET, RequestMethod.POST})
 	public String deleteForm() {
@@ -84,42 +64,18 @@ public class GuestController{
 		
 		System.out.println(guestVo.toString());
 		
-		//GuestDao guestDao = new GuestDao();
-		int count = guestDao.guestDelete(guestVo);
+		int result = guestDao.guestDelete(guestVo);
 		
-		if(count == 1) {
-			//성공 /* redirect */
+		if(result == 1) {
+			//성공 redirect
 			return"redirect:/guestbook/list";
 		} else {
 			
-			model.addAttribute("result", count);
-			//실패 /* 포워드 */
+			model.addAttribute("result", result);
+			//실패 포워드 
 			return "deleteForm";
 		}
+		
 	}
-	
-	// @RequestParam
-	@RequestMapping(value="/delete2",method= {RequestMethod.GET, RequestMethod.POST})
-	public String delete2(@RequestParam("no")int no, @RequestParam("password")String password, Model model) {
-		System.out.println("delete2");
 		
-		GuestVo guestVo = new GuestVo(no,password);
-		
-		System.out.println(guestVo.toString());
-		
-		//GuestDao guestDao = new GuestDao();
-		int count = guestDao.guestDelete(guestVo);
-		
-		if(count == 1) {
-			//성공 /* redirect */
-			return"redirect:/guestbook/list";
-		} else {
-			
-			model.addAttribute("result", count);
-			//실패 /* 포워드 */
-			return "deleteForm";
-		}
-
-	}
-	
 }
